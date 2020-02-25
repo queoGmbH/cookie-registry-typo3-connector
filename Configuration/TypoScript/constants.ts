80190c97768b56f267aeaa74da957536d78da471
Maik Starke <m.starke@queo-group.com>
2020-02-25 13:03:35 +0100

    * add cookie-settings-manager.min.js * adjust default configuration-path * require concrete version of cookie-registry

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -15,7 +15,7 @@ plugin.tx_cookieregistryconnector {
 	}
 
 	settings {
-		configurationYamlPath = config/configuration.prod.yml
+		configurationYamlPath = ../config/configuration.yml
 		includeJsFooter {
 			cookieRegistryLib = EXT:hanse_site/Resources/Public/dist/shared/static/cookie-settings-manager.min.js
 			cookieRegistryInit = EXT:cookie_registry_typo3_connector/Resources/Public/javascripts/initCookieRegistry.js
