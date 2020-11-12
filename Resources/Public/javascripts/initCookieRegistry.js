1830d369c01c15553e887ae56ac3c8f97c535d01
Michael Grundkötter <mg@ujamii.com>
2020-11-12 15:50:45 +0100

    [TASK] fixed js warnings in IDE

--- Resources/Public/javascripts/initCookieRegistry.js
+++ Resources/Public/javascripts/initCookieRegistry.js
@@ -1,8 +1,8 @@
 (function () {
-    var randomNumber = Date.now() / 1000 | 0;
-    var baseUrlPath = window.location.href.replace(".html", "");
+    const randomNumber = Date.now() / 1000 | 0;
+    const baseUrlPath = window.location.href.replace(".html", "");
     // add page-type
-    var jsonUrl = extendUrlParameters(baseUrlPath, "type", "12783490");
+    let jsonUrl = extendUrlParameters(baseUrlPath, "type", "12783490");
     // add no_cache
     jsonUrl = extendUrlParameters(jsonUrl, "no_cache", "1");
     // add random number
@@ -18,18 +18,18 @@
      */
     function extendUrlParameters(sourceUrl, parameterName, parameterValue, replaceDuplicates)
     {
-        if ((sourceUrl == null) || (sourceUrl.length == 0)) {
+        if ((sourceUrl == null) || (sourceUrl.length === 0)) {
             sourceUrl = document.location.href;
         }
 
-        var urlParts = sourceUrl.split("?");
-        var newQueryString = "";
+        let urlParts = sourceUrl.split("?");
+        let newQueryString = "";
         if (urlParts.length > 1) {
-            var parameters = urlParts[1].split("&");
-            for (var i = 0; (i < parameters.length); i++) {
-                var parameterParts = parameters[i].split("=");
-                if (!(replaceDuplicates && parameterParts[0] == parameterName)) {
-                    if (newQueryString == "") {
+            const parameters = urlParts[1].split("&");
+            for (let i = 0; (i < parameters.length); i++) {
+                const parameterParts = parameters[i].split("=");
+                if (!(replaceDuplicates && parameterParts[0] === parameterName)) {
+                    if (newQueryString === "") {
                         newQueryString = "?";
                     } else {
                         newQueryString += "&";
@@ -38,7 +38,7 @@
                 }
             }
         }
-        if (newQueryString == "") {
+        if (newQueryString === "") {
             newQueryString = "?";
         } else {
             newQueryString += "&";
